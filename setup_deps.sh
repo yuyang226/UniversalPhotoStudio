6cfd61c3992617d239f1c60e8c8a315c4dd88b7b
Yang Yu <yuyang226@gmail.com>
2012-12-07 23:00:36 +0800

    mavenize the project

--- setup_deps.sh
+++ setup_deps.sh
@@ -1,29 +1,5 @@
 #SlidingMenu
-if [ ! -d "SlidingMenu" ];
-then
-    git clone git@github.com:jfeinstein10/SlidingMenu.git
-else
-	echo "pulling the latest code for SlidingMenu"
-	#cd SlidingMenu
-	#git pull origin master
-fi
+mvn install:install-file -Dfile=dependencies/SlidingMenu/slidingmenu.jar -DpomFile=dependencies/SlidingMenu/pom.xml
 
 # jinstagram
-if [ ! -d "jInstagram" ];
-then
-    git clone git@github.com:sachin-handiekar/jInstagram.git
-else
-	echo "pulling the latest code for jInstagram"
-	#cd jInstagram
-	#git pull origin master
-fi
-
-if [ ! -f jInstagram/jInstagram/pom.xml ]
-then
-	echo "pom.xml for jInstagram is missing"
-    cp dependencies/jInstagram/pom.xml jInstagram/jInstagram
-fi
-
-cd jInstagram/jInstagram
-mvn clean install -DskipTests
-cp target/jInstagram-1.0.0.jar ../../UniversalPhotoStudio/libs
+mvn install:install-file -Dfile=dependencies/jInstagram/jInstagram-1.0.0.jar -DpomFile=dependencies/jInstagram/pom.xml
