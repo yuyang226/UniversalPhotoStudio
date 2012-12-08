e7b4908e1dda290e6276a251d68df945f0f4404b
Yang Yu <yuyang226@gmail.com>
2012-12-08 08:38:02 +0800

    properly mavenized

--- setup_deps.sh
+++ setup_deps.sh
@@ -1,5 +1,5 @@
 #SlidingMenu
-mvn install:install-file -Dfile=dependencies/SlidingMenu/slidingmenu.jar -DpomFile=dependencies/SlidingMenu/pom.xml
+mvn install:install-file -Dfile=dependencies/SlidingMenu/SlidingMenu.apklib -DpomFile=dependencies/SlidingMenu/pom.xml
 
 # jinstagram
 mvn install:install-file -Dfile=dependencies/jInstagram/jInstagram-1.0.0.jar -DpomFile=dependencies/jInstagram/pom.xml
