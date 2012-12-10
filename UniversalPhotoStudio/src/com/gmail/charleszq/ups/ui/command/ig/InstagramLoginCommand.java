e7e4312e5b240a2d7fd977bded42f33f3364219d
Charles Zhang <charleszq@gmail.com>
2012-12-10 15:13:36 +0800

    add instagram auth scope/permission

--- UniversalPhotoStudio/src/com/gmail/charleszq/ups/ui/command/ig/InstagramLoginCommand.java
+++ UniversalPhotoStudio/src/com/gmail/charleszq/ups/ui/command/ig/InstagramLoginCommand.java
@@ -5,6 +5,7 @@ package com.gmail.charleszq.ups.ui.command.ig;
 
 import org.jinstagram.auth.InstagramAuthService;
 import org.jinstagram.auth.oauth.InstagramService;
+import org.jinstagram.model.Scope;
 
 import android.content.Context;
 import android.content.Intent;
@@ -27,10 +28,15 @@ public class InstagramLoginCommand extends AbstractCommand<Object> {
 
 	@Override
 	public boolean execute(Object... params) {
+		StringBuilder sb = new StringBuilder();
+		sb.append(Scope.COMMENTS.toString()).append(" "); //$NON-NLS-1$
+		sb.append(Scope.LIKES.toString()).append(" "); //$NON-NLS-1$
+		sb.append(Scope.RELATIONSHIPS.toString());
 		InstagramService service = new InstagramAuthService()
 				.apiKey(IConstants.INSTAGRAM_CLIENT_ID)
 				.apiSecret(IConstants.INSTAGRAM_CLIENT_SECRET)
-				.callback(IConstants.IG_CALL_BACK_STR).build();
+				.callback(IConstants.IG_CALL_BACK_STR).scope(sb.toString())
+				.build();
 		String authUrl = service.getAuthorizationUrl(null);
 		Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse(authUrl));
 		mContext.startActivity(i);
